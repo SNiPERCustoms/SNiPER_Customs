3f0b8ca2b234afe7d3c3cfc344d597a09e43cd45
SNiPERCustoms <107870352+SNiPERCustoms@users.noreply.github.com>
2022-06-22 20:53:49 +0530

    Personal Website For Me

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    
+    $('.navbar-toggler-icon').click(function(){
+        $('.navbar-nav').toggleClass("active");
+        $('.avbar-toggler').toggleClass("active")
+    })
+
+    var typed = new Typed(".typing", {
+        strings: ["Web Designer", "Content Writer", "Web Devoloper", "freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+    // toggle menu/navbar script 
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.menu-btn i').toggleClass("active")
+
+    });
+
+    
+
+});
+
